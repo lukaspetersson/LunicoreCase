de3ee56a28ace485d8bfdec0d7ff1b6cb7d739fa
Lukas <Lukas.petersson.1999@gmail.com>
2019-10-31 18:33:20 +0100

    background

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,74 +4,79 @@ import person_icon from "./../assets/person_icon.svg"
 
 
 class LoginPage extends React.Component {
-    constructor(props){
-            super(props)
-			this.state = {
-                loginVisibility: false,
-                loginSize:null,
-            }
-    }
+	constructor(props){
+		super(props)
+		this.state = {
+			loginVisibility: false,
+			loginSize:null,
+		}
+		this.login = this.login.bind(this)
 
+	}
 
+	login(){
+		this.setState({loginVisibility:false})
+			console.log("HHHHHHHåpjonå")
+	}
 	componentDidMount(){
-        this.resizeWindow()
-         window.addEventListener('resize', this.resizeWindow)
-    }
+		this.resizeWindow()
+		window.addEventListener('resize', this.resizeWindow)
+	}
 
-    componentWillUnmount() {
-      window.removeEventListener('resize', this.resizeWindow)
-    }
-    resizeWindow = ()=>{
-        if(window.innerWidth < 600){
-            this.setState({
-              loginSize:8,
-            })
-        }else if(window.innerWidth < 800){
-          this.setState({
-            loginSize:15,
-          })
-        }else{
-            this.setState({
-              loginSize:30,
-            })
-        }
-    }
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.resizeWindow)
+	}
+	resizeWindow = ()=>{
+		if(window.innerWidth < 600){
+			this.setState({
+				loginSize:8,
+			})
+		}else if(window.innerWidth < 800){
+			this.setState({
+				loginSize:15,
+			})
+		}else{
+			this.setState({
+				loginSize:30,
+			})
+		}
+	}
 
-    render() {
-
+	render() {
+			var showLogin = 0;
+			var visible = "hidden";
 			if(this.state.loginVisibility){
-				return (
-
-					<form style={{right: this.state.loginSize+"vw", left: this.state.loginSize+"vw", bottom: 20-this.state.loginSize/6+"vh"}}>
-				    <div className="container" >
-				      <label htmlFor="uname"><b>Username</b></label>
-				      <input type="text" placeholder="Enter Username" name="uname" required/>
-
-				      <label htmlFor="psw"><b>Password</b></label>
-				      <input type="password" placeholder="Enter Password" name="psw" required/>
-
-				      <button type="submit">Login</button>
-				      <label>
-				        <input type="checkbox" checked="checked" onChange={()=>console.log("einpov")} name="remember"/> Remember me
-				      </label>
-				    </div>
-
-				    <div className="container">
-				      <button type="button" className="cancelbtn" onClick={() => this.setState({loginVisibility:false})}>Cancel</button>
-				      <span className="psw">Forgot password?</span>
-				    </div>
-				  </form>
-		        );
-			}else{
-				return (
+				showLogin=1
+				visible = "visible"
+				var background = <div className="outsideSpaceLogin" onClick={() => this.setState({loginVisibility:false})}></div>
+			}
+			return (
+				<div>
+					{background}
 					<div className="loginBtn"  onClick={() => this.setState({loginVisibility:true})}>
 						<img alt="" src={person_icon} />
 						<span>Login</span>
 					</div>
-				);
-			}
+					<form style={{opacity: showLogin, visibility: visible, right: this.state.loginSize+"vw", left: this.state.loginSize+"vw", bottom: 20-this.state.loginSize/6+"vh"}}>
+						<div className="container" >
+							<label htmlFor="uname"><b>Username</b></label>
+							<input type="text" placeholder="Enter Username" name="uname" required/>
 
-    }
+							<label htmlFor="psw"><b>Password</b></label>
+							<input type="password" placeholder="Enter Password" name="psw" required/>
+
+							<button type="button" onClick={()=>{this.login()}}>Login</button>
+						</div>
+
+						<div className="container">
+							<button type="button" className="cancelbtn" onClick={() => this.setState({loginVisibility:false})}>Cancel</button>
+							<span className="psw">Forgot password?</span>
+						</div>
+					</form>
+				</div>
+			);
+
+	}
 }
 
 export default LoginPage;

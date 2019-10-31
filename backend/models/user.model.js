1a6dbe78960d5ae29c2fd2457ea12824a05d79eb
Lukas <Lukas.petersson.1999@gmail.com>
2019-10-31 18:38:50 +0100

    user route

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-	name: {type: String, required: true, trim: true},
+	username: {type: String, required: true, trim: true},
 	password: {type: String, required: true, trim: true}
 });
 
